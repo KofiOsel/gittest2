98208cc566c22dd396724abf9a46cfbb59cf7df2
Arsh <60395168+ARSH5588@users.noreply.github.com>
2021-12-13 23:46:53 -0500

    Update auto-deploy.py

--- auto-deploy.py
+++ auto-deploy.py
@@ -1,8 +1,8 @@
 import time
 import os
 
-path = '/home/arsh/staticWebsite'
-cmd = 'cd /home/arsh/staticWebsite; sudo bundle exec jekyll serve'
+path = '/home/arsh/my_website2'
+cmd = 'cd /home/arsh/my_website2; sudo bundle exec jekyll serve'
 while os.path.isdir(path):
     os.system(cmd)
     time.sleep(300)
